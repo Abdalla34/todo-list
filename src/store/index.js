ff9828f8d841c41bf6087149e7c6859eced52444
Abdalla34 <abogabla88@gmail.com>
2024-10-20 01:06:58 +0300

    todolist

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,24 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    tasks: JSON.parse(localStorage.getItem("task")) || [],
+  },
+  mutations: {
+    ADTOLIST(state, task) {
+      state.tasks.unshift(task);
+      localStorage.setItem("task", JSON.stringify(state.tasks));
+    },
+    DELETED(state, index) {
+      state.tasks.splice(index, 1);
+      localStorage.setItem("task", JSON.stringify(state.tasks));
+    },
+    UPDATECOMPLETED(state, index) {
+      state.tasks[index].completed = !state.tasks[index].completed;
+      localStorage.setItem("task", JSON.stringify(state.tasks));
+    },
+  },
   getters: {},
-  mutations: {},
   actions: {},
   modules: {},
 });
